dd11a22c467e9cebdf45e570d5e93c2255903106
Neel-Barot <neel.barot@bytestechnolab.com>
2024-09-06 15:56:40 +0530

    added extra code in the strapi

---
+++ config/middlewares.js
@@ -0,0 +1,21 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'https://dl.airtable.com', 'https://cdn.shopify.com'],
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
